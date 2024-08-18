626d0131ecd00037935d826fde60149b806899c9
Vitaly Trakhtenberg <webdev@tca.by>
2024-08-19 00:12:33 +0300

    Version 4 of solution - back to lambda, but another

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -1,27 +1,21 @@
 package telran.stream;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamTasks
 {
     public static int[] shuffle(int [] src_arr)
     {
-        int[] shuffled_arr = new int[src_arr.length];
-        boolean[] filled = new boolean[src_arr.length];
+        int[] shuffled_arr = src_arr.clone();
         Random random = new Random();
 
-        IntStream.of(src_arr).forEach(value -> {
-            int index;
-            do {
-                index = random.nextInt(shuffled_arr.length);
-            } while (filled[index]);
-            shuffled_arr[index] = value;
-            filled[index] = true;
-        });
+        IntStream.iterate(shuffled_arr.length - 1, i -> i > 0, i -> i - 1)
+                .forEach(i -> {
+                    int j = random.nextInt(i + 1);
+                    int temp = shuffled_arr[i];
+                    shuffled_arr[i] = shuffled_arr[j];
+                    shuffled_arr[j] = temp;                });
 
         return shuffled_arr;
     }
