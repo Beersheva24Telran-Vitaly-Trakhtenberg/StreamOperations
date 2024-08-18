2102293b95c509a55a5dce0ae1cd64847181e339
Vitaly Trakhtenberg <webdev@tca.by>
2024-08-18 23:22:44 +0300

    Version 2 of solution

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -1,23 +1,23 @@
 package telran.stream;
 
-import java.util.Random;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class StreamTasks
 {
     public static int[] shuffle(int [] src_arr)
     {
-        Random random = new Random();
-        int[] copy = src_arr.clone();
-
-        return IntStream.range(0, copy.length)
-                .map(i -> {
-                    int rnd_index = random.nextInt(src_arr.length - i) + i;
-                    int temp = copy[rnd_index];
-                    copy[rnd_index] = copy[i];
-                    copy[i] = temp;
-                    return copy[i];
-                })
+        return Arrays.stream(src_arr)
+                .boxed()
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        list -> {
+                            Collections.shuffle(list);
+                            return list.stream();
+                        }
+                ))
+                .mapToInt(Integer::intValue)
                 .toArray();
     }
 }
